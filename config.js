f9eea4d7b20f19fb71187fbb4a3d2f91fcdb0102
HuangJianHong <18030221649@163.com>
2025-05-15 14:38:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ var config = {
      */
     imgs: {
         "远方的那位特别的小姐姐": "./imgs/xiaokeai.jpg",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        // "心爱的小可爱": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
